16aee3fd3de6d2b81cae91f09dedd9c1e53c0f20
SignpostMarv <SignpostMarv@cb8136aa-5222-11de-bc8f-bda5044519d4>
2009-06-11 18:39:47 +0000

    splitting InfiniminerMessage into its own file

--- branches/Marvulous Mod/code/InfiniminerClient/PropertyBag.cs
+++ branches/Marvulous Mod/code/InfiniminerClient/PropertyBag.cs
@@ -106,36 +106,6 @@ namespace Infiniminer
         RadarSwitch,
     }
 
-    public enum InfiniminerMessage : byte
-    {
-        MapInfo,                // server information (currently only mapsize)
-        BlockBulkTransfer,      // x-value, y-value, followed by mapsize bytes of blocktype ; 
-        BlockSet,               // x, y, z, type
-        UseTool,                // position, heading, tool, blocktype 
-        SelectClass,            // class
-        ResourceUpdate,         // ore, cash, weight, max ore, max weight, team ore, team A cash, team B cash: ReliableInOrder1
-        DepositOre,
-        DepositCash,
-        WithdrawOre,
-        TriggerExplosion,       // position
-        
-        PlayerUpdate,           // (uint id for server), position, heading, current tool, animate using (bool): UnreliableInOrder1
-        PlayerJoined,           // uint id, player name :ReliableInOrder2
-        PlayerLeft,             // uint id              :ReliableInOrder2
-        PlayerSetTeam,          // (uint id for server), byte team   :ReliableInOrder2
-        PlayerDead,             // (uint id for server) :ReliableInOrder2
-        PlayerAlive,            // (uint id for server) :ReliableInOrder2
-        PlayerPing,             // uint id
-
-        ChatMessage,            // byte type, string message : ReliableInOrder3
-        GameOver,               // byte team
-        PlaySound,              // byte sound, bool isPositional, ?Vector3 location : ReliableUnordered
-        TriggerConstructionGunAnimation,
-        SetBeacon,              // vector3 position, string text ("" means remove)
-        TeamConfig,             // byte team, string name, string color, string blood
-        compatibleClient,       // string url to download page (preferably web page, not direct link)
-    }
-
     public class Beacon
     {
         public string ID;
